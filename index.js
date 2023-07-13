f84785b6f81eed6ddaa73fa755b7d7707765f78f
ChristosPetrou <c.petrou@tiomarkets.com>
2023-07-13 09:34:22 +0300

    now it only can be used only with proxy ip

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ var cors = require("cors");
 const mongoose = require("mongoose");
 const users = require("./routes/users.js");
 const PORT = 4000;
+let whiteListIp = ["69.6.31.74"];
 
 const main = async () => {
   const app = express();
   app.use(express.json());
 
   app.use(cors());
+
+  app.use((req, res, next) => {
+    const ipAddress = req.header("x-forwarded-for") || req.socket.remoteAddress;
+
+    // const forwardedFor = req.headers["x-forwarded-for"];
+    // const clientIp = req.ip;
+    console.log("ip", ipAddress);
+
+    let isWhiteListed = whiteListIp.includes(ipAddress);
+
+    if (isWhiteListed) {
+      next();
+    } else {
+      res.status(403).send("Forbidden");
+    }
+  });
 
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("Connected!");
--- rateLimiter.js
+++ rateLimiter.js
@@ -2,9 +2,9 @@ const rateLimit = require("express-rate-limit");
 
 // Define the rate limiter
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 10 * 60 * 1000, // 10 minutes
   max: 5, // limit each IP to 5 requests per windowMs
-  message: "Too many requests, please try again after 15 minutes.",
+  message: "Too many requests, please try again after 19 minutes.",
 });
 
 // Then, export this as a middleware
